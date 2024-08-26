828f12d8505e6cee426bb9bcd49be67c8c7d88c8
Sebastián Flores <sfa120997@gmail.com>
2024-08-26 16:40:12 -0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
